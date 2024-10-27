7b08576ef6f2772dfb19826da9ca63d2effa45e9
Arushi Ajmani <arushiajmani0145@gmail.com>
2024-10-27 15:29:27 +0530

    final

--- OOP/src/main/java/com/example/oop/HelloApplication.java
+++ OOP/src/main/java/com/example/oop/HelloApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
-        SnakeAndLadders game = new SnakeAndLadders();
+        SnakeAndLaddersApp game = new SnakeAndLaddersApp();
         Scene scene = new Scene(game.createBoard(), 600, 600);
         stage.setTitle("Snake and Ladder");
         stage.setScene(scene);
